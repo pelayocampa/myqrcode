f7e0aa5c25ab1dde2ad0bece8a16328724d77070
pelayocampa <pelayo@pelayocampa>
2022-09-22 13:28:39 +0200

    Focus

--- api/index.py
+++ api/index.py
@@ -3,25 +3,26 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import json
 from random import randint
+from os.path import join
 
 
 
 class handler(BaseHTTPRequestHandler):
 
-    '''def get_fortune_cookie():
-        with open('./fortune-cookie.json') as f:
+    def get_fortune_cookie():
+        with open(join('data','fortune-cookie.json'),'r') as f:
             readed_data = f.read()
 
         data = json.loads(readed_data)
         data_len = len(data)
         random_position = randint(0, data_len - 1)
-        return data[random_position]'''
+        return data[random_position]
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        message = "<html><head><title>Tested</title></head><body>Hola</body></html>"
+        message = self.get_fortune_cookie()
         self.wfile.write(message)
         return

e7e3c80ba3ccb187ba49ea303ce4914009a5d381
pelayocampa <pelayo@pelayocampa>
2022-09-22 12:10:24 +0200

    correccion y respeto

--- api/index.py
+++ api/index.py
@@ -3,12 +3,12 @@ from urllib import parse
 import json
 from random import randint
 
-url = "https://raw.githubusercontent.com/reggi/fortune-cookie/master/fortune-cookie.json"
+
 
 class handler(BaseHTTPRequestHandler):
 
     def get_fortune_cookie():
-        file = open("fortune-cookie.json", "r")
+        file = open("fortune-cookie.txt", "r")
         data = json.loads(file)
         data_len = len(data)
         random_position = randint(0, data_len - 1)

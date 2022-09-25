a484d2ed488c6558c8c6eb183f5df8499d50bb77
pelayocampa <pelayo@pelayocampa>
2022-09-25 17:50:04 +0200

    Style

--- api/test.py
+++ api/test.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
